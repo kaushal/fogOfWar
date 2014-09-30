2b5683806f6b7445561e44069d6ce4f1cf88ac19
Kaushal Parikh <kaushalp88@gmail.com>
2014-09-30 16:38:53 -0400

    add numbering for positions

--- generateWorlds/generate.py
+++ generateWorlds/generate.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     left = None
     right = None
@@ -5,17 +7,21 @@ class Node:
     down = None
     value = '-'
 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 def generateMap():
-    bottomLeft = Node()
+    bottomLeft = Node(0, 0)
     currentNode = bottomLeft
 
     # generate a base layer
     for i in range(100):
-        currentNode.right = Node()
+        currentNode.right = Node(i + 1, 1)
         currentNode.right.left = currentNode
         currentNode = currentNode.right
 
-    bottomLeft.up = Node()
+    bottomLeft.up = Node(1, 1)
     bottomLeft.up.down = bottomLeft
 
     currentNodeNewRow = bottomLeft.up
@@ -24,8 +30,12 @@ def generateMap():
     newBottomLeft = currentNodeNewRow
 
     for i in range(100):
+        count = 0
         while currentNodePrevRow != None:
-            current = Node()
+            current = Node(count, i + 1)
+            randVal = random.randint(0, 10)
+            if randVal < 3:
+                current.value = 'B'
             currentNodeNewRow.right = current
             currentNodeNewRow.right.left = currentNodeNewRow
 
@@ -35,8 +45,9 @@ def generateMap():
 
             currentNodeNewRow = currentNodeNewRow.right
             currentNodePrevRow = currentNodePrevRow.right
+            count += 1
 
-        newBottomLeft.up = Node()
+        newBottomLeft.up = Node(0, i + 2)
 
         currentNodeNewRow = newBottomLeft.up
         currentNodePrevRow = newBottomLeft
